30b0021ca8f4b02156bf30efe0bc7d6cd0fd7fac
roelandp <dnaleor@gmail.com>
2017-08-26 15:22:40 +0200

    Added golos chain parameters

--- steembase/chains.py
+++ steembase/chains.py
@@ -8,6 +8,13 @@ known_chains = {
         "sbd_symbol": "SBD",
         "vests_symbol": "VESTS",
     },
+    "GOLOS": {
+        "chain_id": "782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12",
+        "prefix": "GLS",
+        "steem_symbol": "GOLOS",
+        "sbd_symbol": "GBG",
+        "vests_symbol": "VESTS",
+    },    
     "TEST": {
         "chain_id": "18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e",
         "prefix": "TST",
